1ee1f955ce0e8ce8bb84faa403d10c1d86f9604c
fgutzy <ferdinand.gutzy@gmx.de>
2025-11-05 19:42:23 +0100

    works to create exe (gson in module-java)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.flappybird {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
+    requires com.google.gson;
 
     opens org.example.flappybird to javafx.fxml;
     exports org.example.flappybird;
